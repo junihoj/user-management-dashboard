03ca4fed15e36ff1f1b8f03c9a78816272c49d31
junihoj <ezeonyekachukwu98@gmail.com>
2025-05-03 13:53:48 +0100

    initial project setup

---
+++ app/(auth)/layout.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const AuthLayout = ({ children }: React.PropsWithChildren) => {
+  return <React.Fragment>{children}</React.Fragment>;
+};
+
+export default AuthLayout;
